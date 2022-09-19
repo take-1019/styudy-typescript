053301991cccb41dffb61cbaae0e27db8cea317e
take-1019 <tkchtmy@gmail.com>
2022-09-19 19:35:38 +0900

    ✨ TODOリストを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,71 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { ChangeEventHandler, FC, useState } from "react";
+
+type Todo = {
+	id: number;
+	label: string;
+	isDone: boolean;
+};
 
 const Home: NextPage = () => {
-  return (
-    <div>テスト</div>
-  )
-}
+	const [text, setText] = useState("");
+	const [todos, setTodos] = useState<Todo[]>([]);
 
-export default Home
+	const input: ChangeEventHandler<HTMLInputElement> = (e) => {
+		setText(e.target.value);
+	};
+
+	const add = () => {
+		setTodos((prevTodos) => {
+			return [...prevTodos, { id: Math.random(), label: text, isDone: false }];
+		});
+		setText("");
+	};
+
+	const toggle: ChangeEventHandler<HTMLInputElement> = (e) => {
+		setTodos((prevTodos) => {
+			return prevTodos.map((todo) => {
+				if (todo.id === Number(e.target.value)) {
+					return { ...todo, isDone: !todo.isDone };
+				}
+				return todo;
+			});
+		});
+	};
+
+	return (
+		<div className="w-96 mx-auto p-20">
+			<h1 className="text-xl font-bold">Todo</h1>
+			<div className="flex gap-x-2">
+				<input type="text" value={text} onChange={input} className="border border-black" />
+				<button onClick={add} className="border border-black shrink-0 gap-x-2">
+					追加
+				</button>
+			</div>
+			<ul className="mt-4 space-y-2">
+				{todos.map((todo) => (
+					<li key={todo.id}>
+						<label className="flex items-center gap-x-2">
+							<input type="checkbox" value={todo.id} checked={todo.isDone} onChange={toggle} />
+							<span>{todo.label}</span>
+						</label>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+const ListItem: FC<{
+	todo: Todo;
+	toggle: ChangeEventHandler<HTMLInputElement>;
+}> = ({ todo, toggle }) => {
+	return (
+		<label className="flex items-center gap-x-2">
+			<input type="checkbox" value={todo.id} checked={todo.isDone} onChange={toggle} />
+			<span>{todo.label}</span>
+		</label>
+	);
+};
+
+export default Home;
